06473025566045ba4818a0b9f82467c592a505b3
Alessandro Ruzza <aleruzza@pop-os.localdomain>
2024-05-28 00:02:09 -0400

    fix loss

--- losses.py
+++ losses.py
@@ -14,7 +14,8 @@ class MSEandFFT(nn.Module):
         mse = mse.mean()
         in_fft = torch.fft.rfft(inputs, axis=2)
         tar_fft = torch.abs(torch.fft.rfft(targets, axis=2))
-        fftaw = self.c_kea*torch.exp(-self.gamma_kea*torch.range(0, tar_fft.shape[2]))
+        fftaw = self.c_kea*torch.exp(-self.gamma_kea*torch.range(0, tar_fft.shape[2]-1))
         fft = (((in_fft-tar_fft)**2).mean(axis=-1)*fftaw).mean()
         return mse*self.wfft + fft*self.wfft
 
+mdeco
