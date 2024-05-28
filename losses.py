e5f9ad22fe695f4d0e853bf6d4ef72cf3e91db62
Alessandro Ruzza <aleruzza@pop-os.localdomain>
2024-05-27 22:28:34 -0400

    added abs to loss

--- losses.py
+++ losses.py
@@ -13,7 +13,7 @@ class MSEandFFT(nn.Module):
         mse = (inputs-targets)**2
         mse = mse.mean()
         in_fft = torch.fft.rfft(inputs, axis=2)
-        tar_fft = torch.fft.rfft(targets, axis=2)
+        tar_fft = torch.abs(torch.fft.rfft(targets, axis=2))
         fftaw = self.c_kea*torch.exp(-self.gamma_kea*torch.range(0, tar_fft.shape[2]))
         fft = (((in_fft-tar_fft)**2).mean(axis=-1)*fftaw).mean()
         return mse*self.wfft + fft*self.wfft
