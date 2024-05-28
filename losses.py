ed69c5bc59f4ad2d01e4dc0194ca4e0676372da9
Alessandro Ruzza <aleruzza@pop-os.localdomain>
2024-05-28 00:05:53 -0400

    fix

--- losses.py
+++ losses.py
@@ -2,18 +2,19 @@ import torch.nn as nn
 import torch
 
 class MSEandFFT(nn.Module):
-    def __init__(self, wmse=1, wfft=1, c_kea=1, gamma_kea=0.1):
+    def __init__(self, wmse=1, wfft=1, c_kea=1, gamma_kea=0.1, device='cuda'):
         super(MSEandFFT, self).__init__()
         self.wmse = wmse
         self.wfft = wfft
         self.gamma_kea = gamma_kea
         self.c_kea = c_kea
+        self.device = device
 
     def forward(self, inputs, targets):
         mse = (inputs-targets)**2
         mse = mse.mean()
         in_fft = torch.fft.rfft(inputs, axis=2)
         tar_fft = torch.abs(torch.fft.rfft(targets, axis=2))
-        fftaw = self.c_kea*torch.exp(-self.gamma_kea*torch.range(0, tar_fft.shape[2]-1))
+        fftaw = self.c_kea*torch.exp(-self.gamma_kea*torch.range(0, tar_fft.shape[2]-1)).to(self.device)
         fft = (((in_fft-tar_fft)**2).mean(axis=-1)*fftaw).mean()
         return mse*self.wfft + fft*self.wfft
