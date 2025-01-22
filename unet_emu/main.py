6125b97415116d986bf0735d1211e054e7635444
aleruzza <alessandro.ruzza99@gmail.com>
2025-01-22 16:10:02 +0100

    fix

--- unet_emu/main.py
+++ unet_emu/main.py
@@ -58,7 +58,7 @@ def train(params, model):
     #ict, testparam, xtest = get_testset(params)
     testset = TextImageDataset(
                 folder=params['datadir'],
-                label_file='paratest.csv',
+                labels_file='paratest.csv',
                 data_file='datatest.npy',
                 image_size=params['image_size'],
                 shuffle=False,
